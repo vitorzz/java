188dde1189020579755b831a6dbb6aeba8be22c5
vitor.szelli <vitor.szelli@TAT0541225W10-1.senacsp.edu.br>
2018-06-06 17:30:14 -0300

    Modifiquei Calculo do Imc

--- Fundamentos do Java/src/br/com/java/CalculoImc.java
+++ Fundamentos do Java/src/br/com/java/CalculoImc.java
@@ -18,7 +18,24 @@ public class CalculoImc {
         //processamento
         imc = peso / (altura * altura);
         System.out.println("IMC: " + imc);
-        		
+        if (imc > 15 && imc < 16.9) {
+        	System.out.println("Muito abaixo do peso");
+			} else if (imc > 16.9 && imc < 18.4) {
+				System.out.println("Abaixo do peso");
+			} else if (imc > 18.4 && imc < 24.9) {
+				System.out.println("Peso Normal");
+			} else if (imc > 24.9 && imc < 29.9) {
+				System.out.println("Acima do Peso");
+			} else if (imc > 29.9 && imc < 34.9) {
+				System.out.println("Obesidade Grau 1");
+			} else if (imc > 34.9 && imc < 40) {
+				System.out.println("Obesidade Grau 2");
+			} else if (imc < 16) {
+				System.out.println("Extremamente Abaixo do peso, Muito grave");
+			} else {
+				System.out.println("Obesidade Grau 3");
+
+		}		
 	}
 
 }
